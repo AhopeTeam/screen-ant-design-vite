2d5c51aef82dbf3efb56353dea8a4f4f1018166b
[dfy.1998] <[1209213886@qq.com]>
2022-08-21 17:11:13 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,80 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import { setBodySize, refreshScale, getRemFontSize } from './utils/full-screen'
-import { useStore } from 'vuex'
-const store = useStore()
+import HelloWorld from './components/HelloWorld.vue';
+import { setBodySize, refreshScale, getRemFontSize } from './utils/full-screen';
+import { useStore } from 'vuex';
+const store = useStore();
 const dWidth = 1920,
-  dHeight = 1080
+    dHeight = 1080;
 const initScale = () => {
-  setBodySize(dWidth, dHeight)
-  refreshScale(dWidth, dHeight)
-  window.addEventListener(
-    'pageshow',
-    function (e) {
-      if (e.persisted) {
-        // 浏览器后退的时候重新计算
-        refreshScale(dWidth, dHeight)
-      }
-    },
-    false
-  )
-  window.addEventListener(
-    'resize',
-    function (e) {
-      refreshScale(dWidth, dHeight)
-    },
-    false
-  )
-}
+    setBodySize(dWidth, dHeight);
+    refreshScale(dWidth, dHeight);
+    window.addEventListener(
+        'pageshow',
+        function (e) {
+            if (e.persisted) {
+                // 浏览器后退的时候重新计算
+                refreshScale(dWidth, dHeight);
+            }
+        },
+        false,
+    );
+    window.addEventListener(
+        'resize',
+        function (e) {
+            refreshScale(dWidth, dHeight);
+        },
+        false,
+    );
+};
 const initRem = () => {
-  var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
-  window.addEventListener(
-    resizeEvt,
-    (o) => {
-      var s = getRemFontSize(dWidth, dHeight)
+    var resizeEvt =
+        'orientationchange' in window ? 'orientationchange' : 'resize';
+    window.addEventListener(
+        resizeEvt,
+        (o) => {
+            var s = getRemFontSize(dWidth, dHeight);
 
-      document.documentElement.style.fontSize = s + 'px'
-      // 存入vuex 实现比例值共享
-      store.dispatch('rem/setFs', s)
-    },
-    false
-  )
+            document.documentElement.style.fontSize = s + 'px';
+            // 存入vuex 实现比例值共享
+            store.dispatch('rem/setFs', s);
+        },
+        false,
+    );
 
-  document.addEventListener(
-    'DOMContentLoaded',
-    (o) => {
-      var s = getRemFontSize(dWidth, dHeight)
-      document.documentElement.style.fontSize = s + 'px'
-      store.dispatch('rem/setFs', s)
-    },
-    false
-  )
+    document.addEventListener(
+        'DOMContentLoaded',
+        (o) => {
+            var s = getRemFontSize(dWidth, dHeight);
+            document.documentElement.style.fontSize = s + 'px';
+            store.dispatch('rem/setFs', s);
+        },
+        false,
+    );
 
-  setTimeout((o) => {
-    var s = getRemFontSize(dWidth, dHeight)
-    document.documentElement.style.fontSize = s + 'px'
-    store.dispatch('rem/setFs', s)
-  }, 50)
-}
+    setTimeout((o) => {
+        var s = getRemFontSize(dWidth, dHeight);
+        document.documentElement.style.fontSize = s + 'px';
+        store.dispatch('rem/setFs', s);
+    }, 50);
+};
 
-initRem()
+initRem();
 </script>
 
 <template>
-  <router-view></router-view>
+    <router-view></router-view>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+    filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+    filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
---
+++ src/utils/request.ts
@@ -0,0 +1,86 @@
+/*
+ * @Description: 接口统一处理页面
+ * @Author: 丁飞洋
+ * @Date: 2022-08-18 16:15:37
+ * @LastEditTime: 2022-08-21 17:00:40
+ * @LastEditors: 丁飞洋
+ */
+import axios from 'axios'
+// import { MessageBox, Message } from 'element-ui'
+// import store from '@/store'
+// import { getToken } from '@/utils/auth'
+// import Cookies from 'js-cookie'
+
+
+// create an axios instance
+const service: any = axios.create({
+    baseURL: process.env.VUE_APP_URL,
+    withCredentials: true, // send cookies when cross-domain requests
+    timeout: 30000 // request timeout
+})
+// request interceptor
+
+service.interceptors.request.use(
+    <T>(config:T ):T => {
+        // 配置请求头
+        config.headers = {
+            //'Content-Type':'application/x-www-form-urlencoded',   // 传参方式表单
+            'Content-Type': 'application/json;charset=UTF-8',        // 传参方式json
+            'token': '80c483d59ca86ad0393cf8a98416e2a1'              // 这里自定义配置，这里传的是token
+        };
+        return config;
+    },
+    (error: unknown) => {
+        // do something with request error
+        console.log(error) // for debug
+        return Promise.reject(error)
+    }
+)
+
+// response interceptor
+service.interceptors.response.use(
+    (response: any) => {
+        const res: {
+            retcode: string | number,
+            code: string | number,
+            msg: string
+        } = response.data
+        // res = response.data
+        var tag:boolean = false
+        if (typeof res.retcode !== 'undefined') {
+            tag = (res.retcode != 0)
+        }
+        if ((typeof res.code) !== 'undefined') { // mock环境
+            tag = res.code !== 0 && res.code !== 200
+        }
+        //端口报错或失效提示
+        if (tag) {
+            // if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+            //     MessageBox.confirm('你已经登出，不能保持在本页，请重新登录', '您已经登出', {
+            //         confirmButtonText: '重新登录',
+            //         cancelButtonText: '取消登录',
+            //         type: 'warning'
+            //     }).then(() => {
+            //         store.dispatch('user/resetToken').then(() => {
+            //             location.reload()
+            //         })
+            //     })
+            // }
+            return Promise.reject(res.msg || '错误01')
+        } else {
+            return res
+        }
+    },
+    // 接口未请求成功弹出的错误详情
+    (error: unknown) => {
+        console.log('err' + error) // for debug
+        // Message({
+        //     message: error.message,
+        //     type: 'error',
+        //     duration: 5 * 1000
+        // })
+        return Promise.reject(error)
+    }
+)
+
+export default service
