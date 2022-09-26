99647ff53b295d9aa58468ed89dd7e63e746158d
[dfy.1998] <[1209213886@qq.com]>
2022-09-26 09:12:54 +0800

    路由代理配置

--- src/api/text.ts
+++ src/api/text.ts
@@ -2,7 +2,7 @@ import request from '../utils/request'
 // http://47.104.100.118:81/dcflow/manager/dataindex/getList
 export function getCode(params: any) {
     return request({
-        url: 'dcflow/manager/dataindex/getList',
+        url: 'manager/dataindex/getList',
         method: 'post',
         data: params
     })
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
@@ -15,8 +14,8 @@ export default defineConfig({
     server: {
         //反向代理配置，注意rewrite写法，开始没看文档在这里踩了坑
         proxy: {
-            '/dcflow': {
-                target: 'http://47.104.100.118:8080/dcflow', //代理接口
+            '/manager': {
+                target: 'http://47.104.100.118:8080', //代理接口
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ''),
             },
